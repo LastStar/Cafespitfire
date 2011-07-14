ae535fb85e943b687ea9a4ad6cf2dcb3e3184b92
Josef Pospíšil <josef.pospisil@laststar.eu>
2011-07-14 09:37:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CafespitfireCz::Application.config.secret_token = '4f0ac9e0d4f4a47d5684566df46a9acea78599b509cbc2124b92e5af8daee85d146e68b67b7649035e3b4588211ac63bbe55b78de3252fb65078719f6c33c0b3'
